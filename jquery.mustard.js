6b3294b453dee38b791f2811e4d39c3504ee59f1
Ralph Holzmann <ralphholzmann@gmail.com>
2010-09-21 15:07:20 -0500

    Update position resize fix

--- jquery.mustard.js
+++ jquery.mustard.js
@@ -462,7 +462,6 @@
 			
 			},
 			updatePosition : function() {
-				console.log(data.id);
 				if ( data ) {
 					if (data.elements.target.is(':hidden')) {
 						methods.hide.call(data.elements.target);
